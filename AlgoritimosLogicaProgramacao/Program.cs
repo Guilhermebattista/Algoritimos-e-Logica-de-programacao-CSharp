7c71af7d08957c95b420eb4e89c7ae1448d21308
Guilherme Batista Brito <guizeravlogs2355@gmail.com>
2023-08-30 15:39:05 -0300

    Inicializamos o curso

---
+++ AlgoritimosLogicaProgramacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgoritmosLogicaProgramacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura = 1.6558959589498499;
+            int idade = 21;
+            string nome = "Guilherme";
+
+            Console.WriteLine(altura.ToString("F2"));
+            Console.WriteLine(altura.ToString("F3"));
+            Console.WriteLine("A idade do funcionario é :" + idade + " E sua altura é: " + altura);
+        }
+    }
+}
